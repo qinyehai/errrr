224841e998332158b1f8922c01075206c0491708
WenhanSitu <wenhansitu@gmail.com>
2012-09-24 21:21:34 +0800

    Add user init

--- LBSGameDemo/LBSGameDemo/models/User.m
+++ LBSGameDemo/LBSGameDemo/models/User.m
@@ -10,4 +10,21 @@
 
 @implementation User
 
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        _userId = [[NSNumber alloc] init];
+        _userName = [[NSString alloc] init];
+        _level = [[NSNumber alloc] init];
+        _hp = [[NSNumber alloc] init];
+        _mp = [[NSNumber alloc] init];
+        _exp = [[NSNumber alloc] init];
+        _equipments = [[NSMutableArray alloc] init];
+        _items = [[NSMutableArray alloc] init];
+        _cimelias = [[NSMutableArray alloc] init];        
+    }
+    return self;
+}
+
 @end
